3decdd1e6a4160d8beb1d19245f2d066923518dd
wlmgithub <me@here>
2015-06-28 23:07:35 -0700

    Add functions

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
 
+// function declarations
+std::string msg(std::string);
+void println(std::string);
+
+// main func
 int main()
 {
 	for( int a = 10; a <= 11; a = a + 1 )
@@ -7,5 +13,17 @@ int main()
 		std::cout << "Hello World!\n";
 		std::cout << "Well done!  Bravo!!!! \n";
 	}
+
+  println("Calling msg function...");
+  println( msg("I'm the message returned") );
 }
 
+std::string msg(std::string s)
+{
+  return s;
+}
+
+void println(std::string s)
+{
+  std::cout << s << "\n";
+}
