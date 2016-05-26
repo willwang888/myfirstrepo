ae9032ab29700ac873455015daa3951b6bcf3b91
wlmgithub <wangliming@gmail.com>
2016-05-26 01:47:54 +0000

    Add play_with_bisect.py

---
+++ play/py/scripts/play_with_bisect.py
@@ -0,0 +1,47 @@
+#
+# http://www.allitebooks.com/fluent-python/
+#  page #44
+#
+
+
+import bisect
+import sys
+
+HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
+NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
+ROW_FMT = '{0:2d} @ {1:2d}    {2}{0:<2d}'
+
+def demo(bisect_fn):
+  for needle in reversed(NEEDLES):
+    position = bisect_fn(HAYSTACK, needle)
+    offset = position * '  |'
+    print(ROW_FMT.format(needle, position, offset))
+
+if __name__ == '__main__':
+  if sys.argv[-1] == 'left':
+    bisect_fn = bisect.bisect_left
+  else:
+    bisect_fn = bisect.bisect
+  print('DEMO:', bisect_fn.__name__)
+  print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
+  demo(bisect_fn)
+
+
+
+"""
+$ python3 play_with_bisect.py
+DEMO: bisect
+haystack ->  1  4  5  6  8 12 15 20 21 23 23 26 29 30
+31 @ 14      |  |  |  |  |  |  |  |  |  |  |  |  |  |31
+30 @ 14      |  |  |  |  |  |  |  |  |  |  |  |  |  |30
+29 @ 13      |  |  |  |  |  |  |  |  |  |  |  |  |29
+23 @ 11      |  |  |  |  |  |  |  |  |  |  |23
+22 @  9      |  |  |  |  |  |  |  |  |22
+10 @  5      |  |  |  |  |10
+ 8 @  5      |  |  |  |  |8
+ 5 @  3      |  |  |5
+ 2 @  1      |2
+ 1 @  1      |1
+ 0 @  0    0
+
+"""
